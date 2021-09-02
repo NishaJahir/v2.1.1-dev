361b577ece818eb48d118289ca201356f4181dfe
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2021-09-02 22:54:33 +0530

    Update ReinitializePayment.php

--- src/Providers/DataProvider/ReinitializePayment.php
+++ src/Providers/DataProvider/ReinitializePayment.php
@@ -2,34 +2,13 @@
 
 namespace Novalnet\Providers\DataProvider;
 
-use Novalnet\Services\PaymentService;
-use Plenty\Modules\Frontend\Contracts\Checkout;
-use Plenty\Modules\Order\Shipping\Countries\Contracts\CountryRepositoryContract;
 use Plenty\Plugin\Templates\Twig;
-use Plenty\Modules\Basket\Contracts\BasketRepositoryContract;
+use Novalnet\Helpers\PaymentHelper;
 
-class ReinitializePayment
+class PaymentMethodReinitializePayment
 {
-    /**
-     * @param Twig $twig
-     * @param BasketRepositoryContract  $basketRepositoryContract
-     * @param PayPalPlusService         $paypalPlusService
-     * @param PaymentService            $paymentService
-     * @param Checkout                  $checkout
-     * @param CountryRepositoryContract $countryRepositoryContract
-     * @return string
-     */
-    public function call(   Twig                        $twig,
-                            BasketRepositoryContract    $basketRepositoryContract,
-                            PaymentService              $paymentService,
-                            Checkout                    $checkout,
-                            CountryRepositoryContract   $countryRepositoryContract)
-    {
-        $content = '';
-        
-            $content = $basketRepositoryContract->load();
-        
-
-        return $twig->render('Novalnet::Reinitialize.Payment', ['content'=>$content]);
-    }
+  public function call(Twig $twig, $arg):string
+  {
+    return $twig->render('Novalnet::PaymentMethodReinitializePayment', ["order" => $arg[0], "paymentMethodId" => 6002]);
+  }
 }
