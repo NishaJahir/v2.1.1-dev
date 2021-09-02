e5503484fdc2ac02e3a4b05b1668caeef2e499a1
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2021-09-02 18:10:31 +0530

    Update NovalnetInvoicePaymentMethod.php

--- src/Methods/NovalnetInvoicePaymentMethod.php
+++ src/Methods/NovalnetInvoicePaymentMethod.php
@@ -146,7 +146,7 @@ class NovalnetInvoicePaymentMethod extends PaymentMethodService
      */
     public function isSwitchableTo(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -156,6 +156,6 @@ class NovalnetInvoicePaymentMethod extends PaymentMethodService
      */
     public function isSwitchableFrom(): bool
     {
-        return false;
+        return true;
     }
 }
