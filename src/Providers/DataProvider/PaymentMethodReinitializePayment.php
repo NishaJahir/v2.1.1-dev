278a6831a11925f6da9a6d7a8c01ac258667ed07
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2021-09-03 13:07:27 +0530

    Update PaymentMethodReinitializePayment.php

--- src/Providers/DataProvider/PaymentMethodReinitializePayment.php
+++ src/Providers/DataProvider/PaymentMethodReinitializePayment.php
@@ -4,11 +4,30 @@ namespace Novalnet\Providers\DataProvider;
 
 use Plenty\Plugin\Templates\Twig;
 use Novalnet\Helpers\PaymentHelper;
+use Plenty\Modules\Order\Models\Order;
+use Plenty\Modules\Payment\Method\Contracts\PaymentMethodRepositoryContract;
+use Plenty\Modules\Payment\Method\Models\PaymentMethod;
+use Plenty\Plugin\Log\Loggable;
 
 class PaymentMethodReinitializePayment
 {
-  public function call(Twig $twig, $arg):string
+  use Loggable;
+  
+  public function call(Twig $twig, $arg)
   {
-    return $twig->render('Novalnet::PaymentMethodReinitializePayment', ["order" => $arg[0], "paymentMethodId" => 6002]);
+    /** @var PaymentMethodRepositoryContract $paymentMethodRepository */
+      $paymentMethodRepository = pluginApp(PaymentMethodRepositoryContract::class);
+      $paymentMethods          = $paymentMethodRepository->allForPlugin('plenty_novalnet');
+      $paymentIds              = [];
+      foreach ($paymentMethods as $paymentMethod) {
+          if ($paymentMethod instanceof PaymentMethod) {
+              $paymentIds[] = $paymentMethod->id;
+          }
+      }
+    $this->getLogger(__METHOD__)->error('methods', $paymentMethods);
+    $this->getLogger(__METHOD__)->error('ids', $paymentIds);
+    return $twig->render('Novalnet::PaymentMethodReinitializePayment', ["order" => $arg[0], 'paymentIds' => $paymentIds]);
   }
 }
+
+
