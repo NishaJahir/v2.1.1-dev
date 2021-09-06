484196bf98eab0dc3612693346658e955f231549
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2021-09-06 19:46:42 +0530

    Update NovalnetRouteServiceProvider.php

--- src/Providers/NovalnetRouteServiceProvider.php
+++ src/Providers/NovalnetRouteServiceProvider.php
@@ -37,7 +37,7 @@ class NovalnetRouteServiceProvider extends RouteServiceProvider
         $router->match(['post', 'get'], 'payment/novalnet/callback', 'Novalnet\Controllers\CallbackController@processCallback');
         $router->post('payment/novalnet/processPayment', 'Novalnet\Controllers\PaymentController@processPayment');
         $router->post('payment/novalnet/paymentResponse', 'Novalnet\Controllers\PaymentController@paymentResponse');
-        $router->get('payment/novalnet/payOrderNow', 'Novalnet\Controllers\PaymentController@payOrderNow');
+        $router->get('payment/novalnet/payOrderNow/', 'Novalnet\Controllers\PaymentController@payOrderNow');
        
     }
 }
