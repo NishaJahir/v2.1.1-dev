68a4d9935b5dc81138bb3d51068159855d9fc10f
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2021-09-20 18:02:28 +0530

    Update PaymentMethodReinitializePayment.php

--- src/Providers/DataProvider/PaymentMethodReinitializePayment.php
+++ src/Providers/DataProvider/PaymentMethodReinitializePayment.php
@@ -41,7 +41,7 @@ class PaymentMethodReinitializePayment
     }
     
     $paymentKey = $paymentHelper->getPaymentKeyByMop($mopId);
-    $orderAmount = $this->paymentHelper->ConvertAmountToSmallerUnit($order['amounts'][0]['invoiceTotal']);
+    $orderAmount = $paymentHelper->ConvertAmountToSmallerUnit($order['amounts'][0]['invoiceTotal']);
     $serverRequestData = $paymentService->getRequestParameters($basketRepository->load(), $paymentKey, false, $orderAmount);
        
     $paymentHelper->logger('order amount', $orderAmount);
